c75ec26545a6fb1473a6467e193ea70e8cc162c3
Benny Rodriguez <brodpr17@gmail.com>
2016-05-21 03:12:02 -0400

    Add UIImage+animatedGIF.h

---
+++ VirtualTourist/VirtualTourist/VirtualTourist-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  VirtualTourist-Bridging-Header.h
+//  VirtualTourist
+//
+//  Created by Benny on 5/21/16.
+//  Copyright © 2016 Benny Rodriguez. All rights reserved.
+//
+
+#ifndef VirtualTourist_Bridging_Header_h
+#define VirtualTourist_Bridging_Header_h
+
+#import "UIImageAnimatedGIF/UIImage+animatedGIF.h"
+
+#endif /* VirtualTourist_Bridging_Header_h */
